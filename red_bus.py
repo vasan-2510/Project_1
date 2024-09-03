e32db677cd3b655cbbf9b0b81edab5deb143dd77
vasan-2510 <vasandheeraj613@gmail.com>
2024-09-03 19:23:00 +0530

    Add files via upload

---
+++ red_bus.py
@@ -0,0 +1,165 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import mysql.connector
+
+
+
+
+#df = pd.read_csv("/home/vasan/Documents/Python/Project/redbus_data.csv")
+#st.write(df)
+
+my_db = mysql.connector.connect(
+    host="database-1.clo4c88gw2vb.ap-south-1.rds.amazonaws.com",
+    user="admin",
+    passwd="Vasandheeraj",
+    database="red_bus",
+    autocommit=True)
+
+
+mycursor = my_db.cursor()
+sql = "select * from bus_routes"
+mycursor.execute(sql)
+data = mycursor.fetchall()
+st.dataframe(data)
+
+def skills_take_away():
+    st.header("Skills Take Away From This Project")
+    st.caption("Selenium Webdriver")
+    st.caption("Python Scripting")
+    st.caption("Data Scraping")
+    st.caption("Streamlit Application")
+    st.caption("Pandas")
+    st.caption("Data Management using MySQL")
+
+def objective():
+    st.header("Objective")
+    st.caption("To Develop a streamlit application to showcase the data scraped from Redbus.in by using Selenium Webdriver stored in MySQL.")
+
+def prerequisites():
+    st.header("Prerequisites")
+    st.write("1. Python Environment: Install Python on your system.")
+    st.write("2. Selenium driver - Chrome")
+    st.write("3. Dependencies: Install Streamlit, Pandas, Selenium, mysql_connector")
+    st.write("4. SQL Database: Set up MySQL database needs to be configured with the local machine login credentials.")
+    st.write("5. Streamlit: Install Streamlit library for running the application.")
+        
+def required_python_libraries():
+    st.header("Required Python Libraries")
+    st.write("The following Python libraries are required for the project:")
+    libraries = ["Selenium", "pandas", "streamlit", "mysql.connector", "datetime", "re"]
+    st.write(libraries)
+
+
+st.sidebar.header("Navigation Menu")
+menu_option = st.sidebar.radio("Choose a page:", ["Home", "Project"])
+
+if menu_option == "Home":
+    def main():
+        # Main layout
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header("Navigation")
+            options = ["About the Developer", "Skills take away From This Project", "Objective", 
+                    "Prerequisites", "Required Python Libraries", "Approach"]
+            choice = st.radio("Go to", options)
+
+        with col2:
+            if choice == "About the Developer":
+                about_the_developer()
+            elif choice == "Skills take away From This Project":
+                skills_take_away()
+            elif choice == "Objective":
+                objective()
+            elif choice == "Prerequisites":
+                prerequisites()
+            elif choice == "Required Python Libraries":
+                required_python_libraries()
+            elif choice == "Approach":
+                Approach()
+    if __name__ == "__main__":
+        main()
+#The project starts from here 
+if menu_option == "Project":
+    lists=[]
+    df=pd.read_csv("csv_files/df.csv")
+    df_orgin = pd.read_csv("origin.csv")
+    origin_list = []
+    df_destination = pd.read_csv("destination.csv")
+    destination_list = []
+    for j,k in df_orgin.iterrows():
+        origin_list.append(k['Origin'])
+    for l,m in df_destination.iterrows():
+        destination_list.append(m['Destination'])
+    origin_set = list(set(origin_list))
+    destination_set = list(set(destination_list))
+    for i,r in df.iterrows():
+        lists.append(r["Route_name"])
+    
+    range_values = st.slider("Select a range of values:", min_value=0, max_value=10000, value=(0, 10000))
+    st.write("You selected a range:", range_values)
+    
+    origin = st.selectbox("Choose From", origin_set)
+    destination = st.selectbox("Choose To", destination_set)
+    # origin = st.selectbox("Select an option",df)
+    conn = connect_mysql()
+    my_cursor = conn.cursor()
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        if st.button("Apply for price sort"):
+            st.write(f'The values between {range_values[0]} and {range_values[1]}')
+            try:
+                query=f'''select Bus_name,Bus_type,Start_time,End_time,Total_duration,
+                            Price,Seats_Available,Ratings,Route_name,Origin,Destination from bus_routess 
+                        where Price Between {range_values[0]} and {range_values[1]} order by Price desc'''
+                my_cursor.execute(query)
+                out = my_cursor.fetchall()
+                def df():
+                    df=pd.DataFrame(out,columns=["Bus_name","Bus_type","Start_time","End_time","Total_duration",
+                                                            "Price","Seats_Available","Ratings","Route_name","Origin","Destination"])
+                    
+                    st.write(df)
+                    st.write(len(df))
+            except:
+                st.write("Error: Fetching the database")
+    
+
+    with col2:
+        if st.button ("Apply for route sort"):
+            try:
+                query=f'''select Bus_name,Bus_type,Start_time,End_time,Total_duration,
+                            Price,Seats_Available,Ratings,Route_name,Origin,Destination from bus_routess 
+                        where Origin = {'"'+origin+'"'} AND Destination = {'"'+destination+'"'}'''
+                my_cursor.execute(query)
+                out = my_cursor.fetchall()
+                def df():
+                    df=pd.DataFrame(out,columns=["Bus_name","Bus_type","Start_time","End_time","Total_duration",
+                                                            "Price","Seats_Available","Ratings","Route_name","Origin","Destination"])
+                    
+                    st.write(df)
+                    st.write(len(df))
+            except:
+                st.write("Error: Fetching the database")
+    with col3:
+        if st.button (" Apply for both route and price sort"):
+            try:
+                query=f'''select Bus_name,Bus_type,Start_time,End_time,Total_duration,
+                                Price,Seats_Available,Ratings,Route_name,Origin,Destination from bus_routess 
+                            where Origin = {'"'+origin+'"'} AND Destination = {'"'+destination+'"'} AND Price Between {range_values[0]} and {range_values[1]} order by Price desc'''
+                my_cursor.execute(query)
+                out = my_cursor.fetchall()
+                def df():
+                    df=pd.DataFrame(out,columns=["Bus_name","Bus_type","Start_time","End_time","Total_duration",
+                                                            "Price","Seats_Available","Ratings","Route_name","Origin","Destination"])
+                    
+                    st.write(df)
+                    st.write(len(df))
+            except:
+                st.write("Error: Fetching the database")
+
+    try:
+        #To display the count of the data fetched
+        df()
+    except:
+        st.write ("Still DataFrame not fetched")
